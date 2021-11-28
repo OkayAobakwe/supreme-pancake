04635ae1692983cae0ea2909c4ea5cf219504a16
aobakwe sometimes <okayaobakwe@gmail.com>
2021-11-28 16:01:39 +0200

    feat(recipes): add recipes page and instructions

--- web/src/components/CategoryThumnail.tsx
+++ web/src/components/CategoryThumnail.tsx
@@ -1,11 +1,19 @@
 import { FC } from "react"
-import { Box, Center, Image, Stack, Text } from "@chakra-ui/react"
+import { 
+  Box, 
+  Center, 
+  Image, 
+  Stack, 
+  Text,
+  Link,
+  LinkBox,
+  LinkOverlay
+} from "@chakra-ui/react"
+import NextLink from "next/link"
 
-type ThumbProps = {
-  name: string,
-  image: string,
-}
-const CategoryThumb: FC<ThumbProps> = ({name, image}: ThumbProps) => {
+import { ThumbProps } from "../types/index"
+
+const CategoryThumb: FC<ThumbProps> = ({name, image, id}: ThumbProps) => {
 
   return (
     <Center py={12} ml={2} mb={6}>
@@ -14,23 +22,31 @@ const CategoryThumb: FC<ThumbProps> = ({name, image}: ThumbProps) => {
         h="full"
         mb={3}
       >
-        <Box
-          rounded={"lg"}
-          height={"230px"}
-        >
-          <Center>
-            <Image 
-              align={"center"}
-              src={image}
-              height={230}
-              width={282}
-              objectFit={'cover'}
-            />
-          </Center>
-        </Box>
-        <Stack align={"center"}>
-          <Text m="auto">{name}</Text>
-        </Stack>
+        <LinkBox>
+          <Box
+            rounded={"lg"}
+            height={"230px"}
+          >
+            <Center>
+              <Image 
+                align={"center"}
+                src={image}
+                height={230}
+                width={282}
+                objectFit={'cover'}
+              />
+            </Center>
+          </Box>
+          <Stack align={"center"}>
+              <Text m="auto">
+                <NextLink href="/recipe/[id]" as={`/recipe/${id}`}>
+                  <LinkOverlay>
+                    {name}
+                  </LinkOverlay>
+                </NextLink>
+              </Text>
+          </Stack>
+        </LinkBox>
       </Box>
     </Center>
   )
